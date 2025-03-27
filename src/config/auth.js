ccc7f2013fdd2832efb6a9579b375352c60b943c
Bryan Botelho <gamezer.top01@gmail.com>
2025-03-27 20:48:04 -0300

    Criado Autenticação

---
+++ src/config/auth.js
@@ -0,0 +1,6 @@
+export default {
+
+ secret: '6e550a642f3d5f2d2a364ab96697080f',
+ expiresIn: '7d',
+
+};
---
+++ src/database/index.js
@@ -0,0 +1,25 @@
+import Sequelize from 'sequelize';
+import databaseConfig from '../config/database.js';
+
+import User from '../app/models/User.js';
+
+const models = [User];
+
+class Database{
+  constructor(){
+    this.init();
+  }
+
+ init(){
+    //Conexão com Banco
+    this.connection = new Sequelize(databaseConfig);
+
+
+
+
+
+    models.map(model => model.init(this.connection));
+ }
+}
+
+export default new Database();
